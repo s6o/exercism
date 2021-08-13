92cac3ec40f24f9fb041726b1af98039c23a1c14
Oliver Sõro <oliver.s6ro@gmail.com>
2021-08-13 20:32:09 +0300

    Add fsharp/accumulate solution.

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,3 +1,9 @@
 ﻿module Accumulate
 
-let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = failwith "You need to implement this function."
+let accumulate (func: 'a -> 'b) (input: 'a list) : 'b list =
+    let rec loop accum remaining =
+        match remaining with
+        | [] -> accum
+        | a :: rest -> loop (accum @ [ func a ]) rest
+
+    loop [] input
