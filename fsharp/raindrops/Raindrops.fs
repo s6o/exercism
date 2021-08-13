5fdae1a15865d8dd352b11ec0195831ba5746e27
Oliver Sõro <oliver.s6ro@gmail.com>
2021-08-13 19:45:54 +0300

    Add fsharp/raindrops solution.

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -1,3 +1,18 @@
 ﻿module Raindrops
 
-let convert (number: int): string = failwith "You need to implement this function."
+let convert (number: int) : string =
+    let modx m x = x % m
+
+    let samples =
+        [ (modx 3, "Pling")
+          (modx 5, "Plang")
+          (modx 7, "Plong") ]
+
+    let sound =
+        samples
+        |> List.map (fun (f, s) -> if f number = 0 then s else "")
+        |> String.concat ""
+
+    match sound.Length with
+    | 0 -> number.ToString()
+    | _ -> sound
