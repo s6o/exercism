4d45d3c5c92512d55fe3ac3caa35f32ccef349a1
Oliver Sõro <oliver.s6ro@gmail.com>
2021-08-14 11:26:14 +0300

    Add fsharp/clock solution.

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -1,9 +1,29 @@
 module Clock
 
-let create hours minutes = failwith "You need to implement this function."
+type CM = { Minutes: int }
 
-let add minutes clock = failwith "You need to implement this function."
+let create (hours: int) (minutes: int) : CM =
+    let cm = hours * 60 + minutes
 
-let subtract minutes clock = failwith "You need to implement this function."
+    match cm with
+    | t when t >= 0 ->
+        { Minutes =
+              if t = 1440 then
+                  0
+              else
+                  (((t / 60) % 24) * 60) + (t % 60) }
+    | _ ->
+        let (hmin, rmin) =
+            if (abs cm % 60) = 0 then
+                (0, 0)
+            else
+                (1, 60 - (abs cm % 60))
 
-let display clock = failwith "You need to implement this function."
+        { Minutes = ((24 - hmin - ((abs cm / 60) % 24)) * 60) + rmin }
+
+let add minutes clock = create 0 (clock.Minutes + minutes)
+
+let subtract minutes clock = create 0 (clock.Minutes - minutes)
+
+let display (clock: CM) : string =
+    sprintf "%02d:%02d" ((clock.Minutes / 60) % 24) (clock.Minutes % 60)
---
+++ fsharp/clock/repl.fsx
@@ -0,0 +1,7 @@
+#load "Clock.fs"
+
+open Clock
+
+let a = create 24 0
+
+let b = create 0 0
