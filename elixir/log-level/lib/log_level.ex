f2163660cdc0af82ac9b63d6c6b1f6002cae4b3e
Oliver Sõro <oliver.s6ro@gmail.com>
2021-09-02 19:01:16 +0300

    Add elixir/log-level solution.

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,38 @@
 defmodule LogLevel do
+  @levels %{
+    # Current
+    false => %{
+      0 => :trace,
+      1 => :debug,
+      2 => :info,
+      3 => :warning,
+      4 => :error,
+      5 => :fatal
+    },
+    # Legacy
+    true => %{
+      1 => :debug,
+      2 => :info,
+      3 => :warning,
+      4 => :error
+    }
+  }
+
+  @alerts %{
+    {false, :error} => :ops,
+    {true, :error} => :ops,
+    {false, :fatal} => :ops,
+    {true, :fatal} => :ops,
+    {true, :unknown} => :dev1,
+    {false, :unknown} => :dev2
+  }
+
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    @levels |> Map.get(legacy?, :unknown) |> Map.get(level, :unknown)
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    key = to_label(level, legacy?) |> (fn l -> {legacy?, l} end).()
+    Map.get(@alerts, key, false)
   end
 end
