46344668ad973700796c5e30ef3e0b0b93f8d566
Oliver Sõro <oliver.s6ro@gmail.com>
2021-09-02 18:15:19 +0300

    Add elixir/language-list solution.

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,40 @@
 defmodule LanguageList do
-  def new() do
-    # Please implement the new/0 function
-  end
+  def new(), do: []
 
-  def add(list, language) do
-    # Please implement the add/2 function
-  end
+  def add(list, language), do: [language | list]
 
   def remove(list) do
-    # Please implement the remove/1 function
+    case list do
+      [] -> []
+      [_ | rest] -> rest
+    end
   end
 
   def first(list) do
-    # Please implement the first/1 function
+    case list do
+      [] -> nil
+      [first | _] -> first
+    end
   end
 
   def count(list) do
-    # Please implement the count/1 function
+    case list do
+      [] -> 0
+      [_ | rest] -> 1 + count(rest)
+    end
   end
 
   def exciting_list?(list) do
-    # Please implement the exciting_list?/1 function
+    case list do
+      [] ->
+        false
+
+      [item | rest] ->
+        if String.downcase(item) == "elixir" do
+          true
+        else
+          exciting_list?(rest)
+        end
+    end
   end
 end
