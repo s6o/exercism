6c4725ac731c34c3dbeefbf7047577493358c481
Oliver Sõro <oliver.s6ro@gmail.com>
2021-09-02 11:28:51 +0300

    Add elixir/freelancer-rates solution.

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,24 @@
 defmodule FreelancerRates do
+  defp daily_hours(), do: 8.0
+  defp billable_days(), do: 22.0
+
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    daily_hours() * hourly_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount - discount / 100 * before_discount
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    billable_days()
+    |> Kernel.*(daily_rate(hourly_rate))
+    |> apply_discount(discount)
+    |> Kernel.ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    daily_spend = hourly_rate |> daily_rate() |> apply_discount(discount)
+    (budget / daily_spend) |> Float.floor(1)
   end
 end
