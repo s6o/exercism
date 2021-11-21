2473035eaea1648deebcaaf375a27b34b759db38
Oliver Sõro <oliver.s6ro@gmail.com>
2021-11-21 21:39:16 +0200

    Add elixir/high-score solution.

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,25 +1,27 @@
 defmodule HighScore do
+  @init 0
+
   def new() do
-    # Please implement the new/0 function
+    %{}
   end
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def add_player(scores, name, score \\ @init) do
+    scores |> Map.put(name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    scores |> Map.delete(name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    scores |> Map.put(name, @init)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    Map.update(scores, name, score, fn s -> s + score end)
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    scores |> Map.keys()
   end
 end
