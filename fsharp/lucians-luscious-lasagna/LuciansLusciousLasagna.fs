ba7fcb804655d9cdd163a33cffb662a9c0cf06b8
Oliver Sõro <oliver.s6ro@gmail.com>
2021-11-21 21:56:11 +0200

    Add fsharp/lucians-luscious-lasanga solution.

--- fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -1,9 +1,10 @@
 module LuciansLusciousLasagna
 
-// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
 
-// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven elapsed = expectedMinutesInOven - elapsed
 
-// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes layer = layer * 2
 
-// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes layer elapsed =
+    preparationTimeInMinutes layer + elapsed
