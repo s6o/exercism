b7fa1f8b37466260dcb3da35bcff811409867f62
Oliver Sõro <oliver.s6ro@gmail.com>
2022-01-02 13:02:13 +0200

    Add fsharp/cars-assemble solution.

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -1,10 +1,23 @@
 module CarsAssemble
 
-let successRate (speed: int): float =
-    failwith "Please implement the 'successRate' function"
+let defaultCarsPerHour = 221
 
-let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+let successRate (speed: int) : float =
+    if speed <= 0 then
+        0.00
+    else if speed >= 1 && speed <= 4 then
+        1.00
+    else if speed >= 5 && speed <= 8 then
+        0.90
+    else if speed = 9 then
+        0.80
+    else
+        0.77
 
-let workingItemsPerMinute (speed: int): int =
-    failwith "Please implement the 'workingItemsPerMinute' function"
+let productionRatePerHour (speed: int) : float =
+    let prediction = speed * defaultCarsPerHour |> float
+    let defectAdjustment = successRate speed
+    prediction * defectAdjustment
+
+let workingItemsPerMinute (speed: int) : int =
+    productionRatePerHour speed / 60.0 |> int
