1dac16aeba58e97ef4926f23e1fc1573ea45d3e7
Oliver Sõro <oliver.s6ro@gmail.com>
2023-02-24 08:44:05 +0200

    Add fixed elixir/luhn solution.
    I must be dyslexic, it took forever to notice that I must parse from
    right to left ;-P

--- elixir/luhn/lib/luhn.ex
+++ elixir/luhn/lib/luhn.ex
@@ -1,43 +1,28 @@
 defmodule Luhn do
-  require Integer
-
   @doc """
   Checks if the given number is valid via the luhn formula
   """
   @spec valid?(String.t()) :: boolean
   def valid?(number) do
-    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    cleaned = number |> String.replace(" ", "")
+    space_cleaned = number |> String.replace(" ", "")
+    digit_cleaned = number |> String.replace(~r/[^\d]/, "")
 
-    if cleaned == "0" do
+    if space_cleaned != digit_cleaned or String.length(digit_cleaned) == 1 do
       false
     else
-      cleaned
-      |> IO.inspect(label: "cleaned")
+      digit_cleaned
       |> String.split(~r{.}, include_captures: true, trim: true)
-      |> IO.inspect(label: "splitted")
-      |> Enum.reduce_while({1, 0}, fn s, {idx, sum} ->
-        is_digit = Enum.member?(digits, s)
+      |> List.foldr({0, 0}, fn s, {idx, sum} ->
+        d = String.to_integer(s)
 
-        if is_digit do
-          d = s |> String.to_integer() |> IO.inspect(label: "digit")
-
-          if idx |> Integer.is_even() do
-            dsum = d * 2
-
-            {:cont, {idx + 1, sum + if(dsum > 9, do: dsum - 9, else: dsum)}}
-            |> IO.inspect(label: "event, digit")
-          else
-            {:cont, {idx + 1, sum + d}} |> IO.inspect(label: "odd, digit")
-          end
+        if rem(idx, 2) == 1 do
+          dsum = d * 2
+          {idx + 1, sum + if(dsum > 9, do: dsum - 9, else: dsum)}
         else
-          {:halt, {idx, sum}}
+          {idx + 1, sum + d}
         end
       end)
-      |> (fn {_, sum} ->
-            IO.inspect(sum, label: "sum")
-            rem(sum, 10) == 0
-          end).()
+      |> (fn {_, sum} -> rem(sum, 10) == 0 end).()
     end
   end
 end
