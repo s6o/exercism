8f1e1ece181e8481784d20f8c2dca721c044371e
Oliver Sõro <oliver.s6ro@gmail.com>
2023-03-25 12:47:07 +0200

    Add elixir/german-sysadmin solution.

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,15 @@
 defmodule Username do
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    username
+    |> Enum.reduce([], fn c, acc ->
+      case c do
+        u when u == ?ä -> acc ++ [?a, ?e]
+        u when u == ?ö -> acc ++ [?o, ?e]
+        u when u == ?ü -> acc ++ [?u, ?e]
+        s when s == ?ß -> acc ++ [?s, ?s]
+        v when v == ?_ or (v >= ?a and v <= ?z) -> acc ++ [v]
+        _ -> acc
+      end
+    end)
   end
 end
