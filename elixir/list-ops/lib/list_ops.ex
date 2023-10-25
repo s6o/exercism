f6abf818bb25d41a387e7903730f0bdf7214f67a
Oliver Sõro <oliver.s6ro@gmail.com>
2023-10-25 20:33:39 +0300

    Update elixir/list_ops requirements and solution.

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -4,47 +4,43 @@ defmodule ListOps do
   # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
   # for adding numbers), but please do not use Kernel functions for Lists like
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+  @type acc :: any()
 
-  @spec count(list) :: non_neg_integer
-  def count(l) do
-    reduce(l, 0, fn _, c -> c + 1 end)
+  @spec append(list, list) :: list
+  def append(a, b), do: reduce(b, reverse(a), fn i, a -> [i | a] end) |> reverse()
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+    reduce(ll, [], fn l, a -> reduce(l, a, fn i, a -> [i | a] end) end) |> reverse()
   end
 
-  @spec reverse(list) :: list
-  def reverse(l) do
-    reduce(l, [], fn i, a -> [i | a] end)
+  @spec count(list) :: non_neg_integer
+  def count(l), do: reduce(l, 0, fn _, c -> c + 1 end)
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+    reduce(l, [], fn i, a -> if(f.(i), do: [i | a], else: a) end) |> reverse()
   end
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
-    reduce(l, [], fn i, a -> [f.(i) | a] end)
-    |> reverse()
+    reduce(l, [], fn i, a -> [f.(i) | a] end) |> reverse()
   end
 
-  @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-    reduce(l, [], fn i, a -> if(f.(i) == true, do: [i | a], else: a) end)
-    |> reverse()
-  end
+  @spec foldl(list, any, (any, any -> any)) :: any
+  def foldl(l, acc, f), do: reduce(l, acc, f)
 
-  @type acc :: any
+  @spec foldr(list, any, (any, any -> any)) :: any
+  def foldr(l, acc, f), do: reverse(l) |> reduce(acc, f)
+
+  @spec reverse(list) :: list
+  def reverse(l), do: reduce(l, [], fn i, a -> [i | a] end)
+
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
+  defp reduce(l, acc, f) do
     case l do
       [] -> acc
       [h | rest] -> reduce(rest, f.(h, acc), f)
     end
-  end
-
-  @spec append(list, list) :: list
-  def append(a, b) do
-    reduce(b, reverse(a), fn i, a -> [i | a] end)
-    |> reverse()
-  end
-
-  @spec concat([[any]]) :: [any]
-  def concat(ll) do
-    reduce(ll, [], fn l, a -> reduce(l, a, fn i, a -> [i | a] end) end)
-    |> reverse()
   end
 end
