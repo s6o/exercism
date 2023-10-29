5ff449007792606915a8549d44c3a59bef49c9dd
Oliver Sõro <oliver.s6ro@gmail.com>
2023-10-29 13:27:26 +0200

    Add python/black-jack assignment with solution.

---
+++ python/black-jack/black_jack.py
@@ -0,0 +1,89 @@
+"""Functions to help play and score a game of blackjack.
+
+How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
+"Standard" playing cards: https://en.wikipedia.org/wiki/Standard_52-card_deck
+"""
+
+
+def value_of_card(card):
+    """Determine the scoring value of a card.
+
+    :param card: str - given card.
+    :return: int - value of a given card.  See below for values.
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 1
+    3.  '2' - '10' = numerical value.
+    """
+
+    return 10 if card in ['J', 'Q', 'K'] else (1 if card == 'A' else int(card))
+
+
+def higher_card(card_one, card_two):
+    """Determine which card has a higher value in the hand.
+
+    :param card_one, card_two: str - cards dealt in hand.  See below for values.
+    :return: str or tuple - resulting Tuple contains both cards if they are of equal value.
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 1
+    3.  '2' - '10' = numerical value.
+    """
+
+    score_one = value_of_card(card_one)
+    score_two = value_of_card(card_two)
+    alternative = (card_one, card_two) if score_one == score_two else card_two
+    return card_one if score_one > score_two else alternative
+
+
+def value_of_ace(card_one, card_two):
+    """Calculate the most advantageous value for the ace card.
+
+    :param card_one, card_two: str - card dealt. See below for values.
+    :return: int - either 1 or 11 value of the upcoming ace card.
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 11 (if already in hand)
+    3.  '2' - '10' = numerical value.
+    """
+
+    ace_in_hand = 'A' == card_one or 'A' == card_two
+    score_diff = 21 - (value_of_card(card_one) + value_of_card(card_two))
+    return 1 if ace_in_hand else (1 if score_diff < 11 else 11)
+
+
+def is_blackjack(card_one, card_two):
+    """Determine if the hand is a 'natural' or 'blackjack'.
+
+    :param card_one, card_two: str - card dealt. See below for values.
+    :return: bool - is the hand is a blackjack (two cards worth 21).
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 11 (if already in hand)
+    3.  '2' - '10' = numerical value.
+    """
+
+    score_one = 11 if 'A' == card_one else value_of_card(card_one)
+    score_two = 11 if 'A' != card_one and 'A' == card_two else value_of_card(card_two)
+    return 21 == score_one + score_two
+
+
+def can_split_pairs(card_one, card_two):
+    """Determine if a player can split their hand into two hands.
+
+    :param card_one, card_two: str - cards dealt.
+    :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
+    """
+
+    return value_of_card(card_one) == value_of_card(card_two)
+
+
+def can_double_down(card_one, card_two):
+    """Determine if a blackjack player can place a double down bet.
+
+    :param card_one, card_two: str - first and second cards in hand.
+    :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
+    """
+
+    total = value_of_card(card_one) + value_of_card(card_two)
+    return total in {9, 10, 11}
