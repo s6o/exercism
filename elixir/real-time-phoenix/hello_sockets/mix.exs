e09dfa2f9e1dcd3d8d0d3b7196e06ef4b8e4f8ac
Oliver Sõro <oliver.s6ro@gmail.com>
2023-11-06 19:07:41 +0200

    Add initial setup for HelloSockets from Real-Time Phoenix.
    * OTP: 22.x (via asdf)
    * Elixir: 1.9.4 (via asdf)
    * Phoenix Framework: 1.3.5 with mix.exs corrected with overrides to get it
      to compile.
    
    Installing the latest security fix version from 1.3.x branch via mix
    archive does not work (any longer).
    
    The work-around was to install the 1.3.4 version:
    
    ```zsh
    mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new-1.3.4.ez
    ```
    
    and then manually correct the mix.exs with packages overrides to:
    
    ```elixir
      defp deps do
        [
          {:phoenix, "1.3.5"},
          {:phoenix_pubsub, "~> 1.0.2"},
          {:plug, "1.6.4", override: true},
          {:mime, "1.3.1", override: true},
          {:phoenix_html, "~> 2.10"},
          {:phoenix_live_reload, "~> 1.0", only: :dev},
          {:cowboy, "~> 1.1.2"},
          {:gettext, "~> 0.15.0"}
        ]
      end
    ```

---
+++ elixir/real-time-phoenix/hello_sockets/mix.exs
@@ -0,0 +1,45 @@
+defmodule HelloSockets.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :hello_sockets,
+      version: "0.0.1",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {HelloSockets.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:phoenix, "1.3.5"},
+      {:phoenix_pubsub, "~> 1.0.2"},
+      {:plug, "1.6.4", override: true},
+      {:mime, "1.3.1", override: true},
+      {:phoenix_html, "~> 2.10"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:cowboy, "~> 1.1.2"},
+      {:gettext, "~> 0.15.0"}
+    ]
+  end
+end
