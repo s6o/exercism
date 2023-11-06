8bc9d162ebadbb788e5bc3651906a450249d3a8f
Oliver Sõro <oliver.s6ro@gmail.com>
2023-11-06 22:38:21 +0200

    Add Real-Time Phoenix HelloSockets ping event handling examples.

--- elixir/real-time-phoenix/hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
+++ elixir/real-time-phoenix/hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -8,4 +8,21 @@ defmodule HelloSocketsWeb.PingChannel do
   def handle_in("ping", _payload, socket) do
     {:reply, {:ok, %{ping: "pong"}}, socket}
   end
+
+  def handle_in("ping", %{"ack_phrase" => ack_phrase}, socket) do
+    {:reply, {:ok, %{ping: ack_phrase}}, socket}
+  end
+
+  def handle_in("ping:" <> phrase, _payload, socket) do
+    {:reply, {:ok, %{ping: phrase}}, socket}
+  end
+
+  # We only handle ping
+  def handle_in("pong", _payload, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_in("ding", _payload, socket) do
+    {:stop, :shutdown, {:ok, %{msg: "shutting down"}}, socket}
+  end
 end
