a9135fc54a59bd6bf18e73b15cf1a793b65f2271
Oliver Sõro <oliver.s6ro@gmail.com>
2023-11-07 07:39:23 +0200

    Add Real-Time Phoenix HelloSockets ping channel interception.

--- elixir/real-time-phoenix/hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
+++ elixir/real-time-phoenix/hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -25,4 +25,11 @@ defmodule HelloSocketsWeb.PingChannel do
   def handle_in("ding", _payload, socket) do
     {:stop, :shutdown, {:ok, %{msg: "shutting down"}}, socket}
   end
+
+  intercept(["request_ping"])
+
+  def handle_out("request_ping", payload, socket) do
+    push(socket, "send_ping", Map.put(payload, "from_node", Node.self()))
+    {:noreply, socket}
+  end
 end
