d261e99d2464f9cd60df2dc95d59a1b852a1af8a
Oliver Sõro <oliver.s6ro@gmail.com>
2023-11-21 20:49:49 +0200

    Add elixir/allergies assignment with solution.

---
+++ elixir/allergies/lib/allergies.ex
@@ -0,0 +1,30 @@
+defmodule Allergies do
+  require Bitwise
+
+  @allergies %{
+    "eggs" => 1,
+    "peanuts" => 2,
+    "shellfish" => 4,
+    "strawberries" => 8,
+    "tomatoes" => 16,
+    "chocolate" => 32,
+    "pollen" => 64,
+    "cats" => 128
+  }
+
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec list(non_neg_integer) :: [String.t()]
+  def list(flags) do
+    @allergies |> Map.keys() |> Enum.filter(&allergic_to?(flags, &1))
+  end
+
+  @doc """
+  Returns whether the corresponding flag bit in 'flags' is set for the item.
+  """
+  @spec allergic_to?(non_neg_integer, String.t()) :: boolean
+  def allergic_to?(flags, item) do
+    Bitwise.band(flags, @allergies[item]) > 0
+  end
+end
