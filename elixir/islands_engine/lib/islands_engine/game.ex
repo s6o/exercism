c5f234225da83c16fbf18b5795c4b716c9ae610c
Oliver Sõro <oliver.s6ro@gmail.com>
2025-10-22 20:51:51 +0300

    Add add elixir/islands_engine game play GenServer.

---
+++ elixir/islands_engine/lib/islands_engine/game.ex
@@ -0,0 +1,109 @@
+defmodule IslandsEngine.Game do
+  use GenServer
+
+  alias IslandsEngine.Board
+  alias IslandsEngine.Coordinate
+  alias IslandsEngine.Guesses
+  alias IslandsEngine.Island
+  alias IslandsEngine.Rules
+
+  @players [:player1, :player2]
+
+  def start_link(player_name) when is_binary(player_name),
+    do: GenServer.start_link(__MODULE__, player_name, name: via_tuple(player_name))
+
+  def add_player(game_pid, player_name) when is_pid(game_pid) and is_binary(player_name),
+    do: GenServer.call(game_pid, {:add_player, player_name})
+
+  def position_island(game_pid, player, shape, row, col)
+      when is_pid(game_pid) and player in @players and is_atom(shape) and is_integer(row) and
+             is_integer(col),
+      do: GenServer.call(game_pid, {:position_island, player, shape, row, col})
+
+  def set_islands(game_pid, player) when is_pid(game_pid) and player in @players,
+    do: GenServer.call(game_pid, {:set_islands, player})
+
+  def guess_coordinate(game_pid, player, row, col)
+      when is_pid(game_pid) and player in @players and is_integer(row) and is_integer(col),
+      do: GenServer.call(game_pid, {:guess_coordinate, player, row, col})
+
+  def init(player_name) do
+    player1 = %{name: player_name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: Rules.new()}}
+  end
+
+  def handle_call({:add_player, player_name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player) do
+      state
+      |> (fn s -> put_in(s.player2.name, player_name) end).()
+      |> (fn s -> put_in(s.rules, rules) end).()
+      |> (fn s -> {:reply, :ok, s} end).()
+    else
+      e -> {:reply, e, state}
+    end
+  end
+
+  def handle_call({:position_island, player, shape, row, col}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(shape, coordinate),
+         {:ok, board} <- Board.position_island(player_board(state, player), island) do
+      state
+      |> (fn s -> put_in(s, [player, :board], board) end).()
+      |> (fn s -> put_in(s.rules, rules) end).()
+      |> (fn s -> {:reply, :ok, s} end).()
+    else
+      e ->
+        {:reply, e, state}
+    end
+  end
+
+  def handle_call({:set_islands, player}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, {:set_islands, player}),
+         true <- Board.all_islands_positioned?(player_board(state, player)) do
+      state
+      |> (fn s -> put_in(s.rules, rules) end).()
+      |> (fn s -> {:reply, :ok, s} end).()
+    else
+      false ->
+        {:reply, {:error, :not_all_islands_positioned}, state}
+
+      e ->
+        {:reply, e, state}
+    end
+  end
+
+  def handle_call({:guess_coordinate, player, row, col}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         opponent_board <- player_board(state, opponent(player)),
+         {hit_or_miss, forested_island, win_status, updated_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
+      state
+      |> (fn s -> put_in(s, [opponent(player), :board], updated_board) end).()
+      |> (fn s ->
+            put_in(
+              s,
+              [player, :guesses],
+              Guesses.add(Map.get(state, player).guesses, hit_or_miss, coordinate)
+            )
+          end).()
+      |> (fn s -> put_in(s.rules, rules) end).()
+      |> (fn s -> {:reply, {hit_or_miss, forested_island, win_status}, s} end).()
+    else
+      e ->
+        {:reply, e, state}
+    end
+  end
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+
+  defp player_board(state, player), do: Map.get(state, player).board
+
+  defp via_tuple(player_name) do
+    {:via, Registry, {Registry.Game, "#{player_name}-#{:rand.uniform(1000)}"}}
+  end
+end
