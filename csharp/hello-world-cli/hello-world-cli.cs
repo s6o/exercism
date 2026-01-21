05d825a5a2f6a429310cc1a93144e6f3cffc5754
Oliver Sõro <oliver.s6ro@gmail.com>
2026-01-21 13:09:12 +0200

    Add csharp/hello-world-cli - single script, C#14 .NET 10.

---
+++ csharp/hello-world-cli/hello-world-cli.cs
@@ -0,0 +1,10 @@
+﻿// Single file programs since .NET 10 and C# 14
+// Shebang is broken, the line below does not work with zsh on MacOS
+// #!/usr/local/share/dotnet/dotnet run
+// 
+// And this version:
+// #!/usr/bin/env dotnet
+// is also broken with zsh on MacOS
+//
+// Running via: dotnet run hello-world-cli.cs
+Console.WriteLine("Hello, World!");
