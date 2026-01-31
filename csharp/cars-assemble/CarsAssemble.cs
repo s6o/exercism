1e62daa3ca66e0755e3e118a434a067f87edef28
Oliver Sõro <oliver.s6ro@gmail.com>
2026-01-31 08:35:38 +0200

    Add csharp/cars-assemble solution.

---
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -0,0 +1,22 @@
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (speed == 0) return 0.0;
+        if (speed >= 1 && speed <= 4) return 1.0;
+        if (speed >= 5 && speed <= 8) return 0.9;
+        if (speed == 9) return 0.8;
+        if (speed == 10) return 0.77;
+        return 0.6;
+    }
+
+    public static double ProductionRatePerHour(int speed)
+    {
+        return speed * 221 * SuccessRate(speed);
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        return (int)(ProductionRatePerHour(speed) / 60);
+    }
+}
