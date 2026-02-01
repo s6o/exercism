4a40f08832210d59509e23b3f62405bf56c55388
Oliver Sõro <oliver.s6ro@gmail.com>
2026-02-01 09:02:24 +0200

    Add csharp/jedliks-toys solution.

---
+++ csharp/jedliks-toys/JedliksToys.cs
@@ -0,0 +1,27 @@
+class RemoteControlCar
+{
+    private int _battery = 100;
+    private int _distance = 0;
+
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+
+    public string DistanceDisplay()
+    {
+        return $"Driven {_distance} meters";
+    }
+
+    public string BatteryDisplay()
+    {
+        return _battery > 0 ? $"Battery at {_battery}%" : "Battery empty";
+    }
+
+    public void Drive()
+    {
+        if (_battery <= 0) return;
+        _battery -= 1;
+        _distance += 20;
+    }
+}
