ee99ac48be30c434c9e0b4f4c2094234cb88d0d7
Oliver Sõro <oliver.s6ro@gmail.com>
2026-02-09 21:15:45 +0200

    Add csharp/secure-munchester-united solution.

---
+++ csharp/secure-munchester-united/SecureMunchesterUnited.cs
@@ -0,0 +1,33 @@
+public class SecurityPassMaker
+{
+    public string GetDisplayName(TeamSupport support) => support switch
+    {
+        Security s when s.GetType() == typeof(Security) => $"{s.Title} Priority Personnel",
+        Staff s => s.Title,
+        TeamSupport ts => "Too Important for a Security Pass",
+    };
+}
+
+/**** Please do not alter the code below ****/
+
+public interface TeamSupport { string Title { get; } }
+
+public abstract class Staff : TeamSupport { public abstract string Title { get; } }
+
+public class Manager : TeamSupport { public string Title { get; } = "The Manager"; }
+
+public class Chairman : TeamSupport { public string Title { get; } = "The Chairman"; }
+
+public class Physio : Staff { public override string Title { get; } = "The Physio"; }
+
+public class OffensiveCoach : Staff { public override string Title { get; } = "Offensive Coach"; }
+
+public class GoalKeepingCoach : Staff { public override string Title { get; } = "Goal Keeping Coach"; }
+
+public class Security : Staff { public override string Title { get; } = "Security Team Member"; }
+
+public class SecurityJunior : Security { public override string Title { get; } = "Security Junior"; }
+
+public class SecurityIntern : Security { public override string Title { get; } = "Security Intern"; }
+
+public class PoliceLiaison : Security { public override string Title { get; } = "Police Liaison Officer"; }
