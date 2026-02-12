8b5589ebca0394c468d440136fbd46ddd16d2d7c
Oliver Sõro <oliver.s6ro@gmail.com>
2026-02-12 11:54:15 +0200

    Add csharp/orm-in-one-go solution.

---
+++ csharp/orm-in-one-go/OrmInOneGo.cs
@@ -0,0 +1,32 @@
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Write(string data)
+    {
+        using (database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        try
+        {
+            this.Write(data);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
