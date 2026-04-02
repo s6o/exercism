8a7986b35f7c18f9abede2a272558bd1b15c7374
Oliver Sõro <oliver.s6ro@gmail.com>
2026-04-02 10:54:17 +0300

    Add go/election-day solution.

---
+++ go/election-day/election_day.go
@@ -0,0 +1,45 @@
+package electionday
+
+import "fmt"
+
+// NewVoteCounter returns a new vote counter with
+// a given number of initial votes.
+//
+//go:fix inline
+func NewVoteCounter(initialVotes int) *int {
+	return new(initialVotes)
+}
+
+// VoteCount extracts the number of votes from a counter.
+func VoteCount(counter *int) int {
+	if counter != nil {
+		return *counter
+	} else {
+		return 0
+	}
+}
+
+// IncrementVoteCount increments the value in a vote counter.
+func IncrementVoteCount(counter *int, increment int) {
+	if counter != nil {
+		*counter += increment
+	}
+}
+
+// NewElectionResult creates a new election result.
+func NewElectionResult(candidateName string, votes int) *ElectionResult {
+	return &ElectionResult{Name: candidateName, Votes: votes}
+}
+
+// DisplayResult creates a message with the result to be displayed.
+func DisplayResult(result *ElectionResult) string {
+	return fmt.Sprintf("%s (%d)", result.Name, result.Votes)
+}
+
+// DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map.
+func DecrementVotesOfCandidate(results map[string]int, candidate string) {
+	votes, exists := results[candidate]
+	if exists {
+		results[candidate] = votes - 1
+	}
+}
